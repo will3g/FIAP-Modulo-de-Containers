a9824d39aa4accb6d07551ccd99d0ec7fd8db323
will3g <wpoliveira@spo.ogmaster.local>
2024-07-25 09:37:34 -0300

    Fix docker compose and deploy files

--- aula 3 - dockerfile e docker compose/video-3.5/deploy.sh
+++ aula 3 - dockerfile e docker compose/video-3.5/deploy.sh
@@ -1,21 +1,14 @@
 # /bin/bash
 
 eval docker build -t mystreamlitconfig -f ./app/Dockerfile ./app
-eval docker build -t myjupyterconfig -f ./configs/jupyter/Dockerfile ./configs/jupyter
-
 eval docker tag mystreamlitconfig gcr.io/<project_id>/mystreamlitconfig:latest
-eval docker tag myjupyterconfig gcr.io/<project_id>/myjupyterconfig:latest
-
 eval docker push gcr.io/<project_id>/mystreamlitconfig:latest
-eval docker push gcr.io/<project_id>/myjupyterconfig:latest
 
 # transformar o docker-compose para o formato kubernates
 eval docker build -t kompose https://github.com/kubernetes/kompose.git\#main && \
      docker run --rm -it -v .:/opt kompose sh -c "cd /opt && kompose convert -f docker-compose-prd.yml"
 
 # comandos para realizar deploy
-eval kubectl apply -f ./jupyter-deployment.yaml
-eval kubectl apply -f ./jupyter-tcp-service.yaml
 eval kubectl apply -f ./mongodb-compass-deployment.yaml
 eval kubectl apply -f ./mongodb-compass-tcp-service.yaml
 eval kubectl apply -f ./mongo-deployment.yaml
