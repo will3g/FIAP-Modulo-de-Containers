2608668ce51c1381e8e62df871431cee21b23d75
will3g <wpoliveira@spo.ogmaster.local>
2024-07-31 06:46:07 -0300

    adding data handler and networks

--- aula 5 - docker network/video-5.3/utils/dump-database.sh
+++ aula 5 - docker network/video-5.3/utils/dump-database.sh
@@ -1,3 +1,3 @@
 # /bin/bash
 
-docker run --rm -it --network mynetwork -v .:/data mongodb-tools sh -c 'mongodump --uri="mongodb://root:root@mongodb/finance" --authenticationDatabase=admin'
+docker run --rm -it --network bridge_network -v .:/data mongodb-tools sh -c 'mongodump --uri="mongodb://root:root@mongodb/finance" --authenticationDatabase=admin'
--- aula 5 - docker network/video-5.3/utils/restore-database.sh
+++ aula 5 - docker network/video-5.3/utils/restore-database.sh
@@ -1,3 +1,3 @@
 # /bin/bash
 
-docker run --rm -it --network mynetwork -v ./dump:/data mongodb-tools sh -c 'mongorestore --uri="mongodb://root:root@mongodb/finance" . --authenticationDatabase=admin'
+docker run --rm -it --network bridge_network -v ./dump:/data mongodb-tools sh -c 'mongorestore --uri="mongodb://root:root@mongodb/finance" . --authenticationDatabase=admin'
