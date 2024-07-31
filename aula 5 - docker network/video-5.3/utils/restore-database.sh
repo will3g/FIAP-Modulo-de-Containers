027a1b4315014b23c29b9baef83e226f54293457
will3g <wpoliveira@spo.ogmaster.local>
2024-07-31 06:54:59 -0300

    renaming folders

--- aula 5 - docker network/video-5.3/utils/restore-database.sh
+++
@@ -1,3 +0,0 @@
-# /bin/bash
-
-docker run --rm -it --network bridge_network -v ./dump:/data mongodb-tools sh -c 'mongorestore --uri="mongodb://root:root@mongodb/finance" . --authenticationDatabase=admin'
