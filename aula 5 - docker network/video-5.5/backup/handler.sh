1f0f62f6b9c97400825cc3b5923af24516538701
will3g <willianpaulo_09@hotmail.com>
2024-10-08 09:53:37 -0300

    fixiing comments and depends_on

--- aula 5 - docker network/video-5.5/backup/handler.sh
+++ aula 5 - docker network/video-5.5/backup/handler.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-
 TIMESTAMP=$(date +"%Y%m%d%H%M%S")
 BACKUP_DIR="/backup/dumps/$TIMESTAMP"
-
 mkdir -p "$BACKUP_DIR"
-
-# Podemos apontar para o GCS, etc... Para mais segurança.
-cp -r ../data/* "$BACKUP_DIR" # Somente para demonstração
-
+cp -r ../data/* "$BACKUP_DIR" # Podemos apontar para o GCS, etc... Para mais segurança. Somente para demonstração
 echo "Backup realizado em $BACKUP_DIR"
